cce1d24719c7b7bb677200ef93aac69633974bc6
Tvkqd <46411203+Tvkqd@users.noreply.github.com>
2022-09-11 18:11:30 -0500

    test search tool

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,89 @@
 <template>
   <v-app>
-  <div id="app">
-    <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <router-link to="/" class="navbar-brand">bezKoder</router-link>
-      <div class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <router-link to="/landing" class="nav-link">Tutorials</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/add" class="nav-link">Add</router-link>
-        </li>
+    <v-app-bar
+      app
+      color="blue lighten-5"
+      dark
+    >
+      <div class="d-flex align-center">
+        <component v-bind:is="component" />
+        <a href="https://www.oc.edu/">
+          <v-img
+          alt="OC Logo"
+          class="shrink mr-2"
+          contain
+          src="OC-Logo.png"
+          transition="scale-transition"
+          width="130"
+        />
+        </a>
+        <v-btn
+        @click="course()"
+        text
+        color="red darken-4"
+        >Courses</v-btn>
+      
+       <!--  <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        /> -->
       </div>
-    </nav>
-    <div class="container mt-3">
-      <router-view />
-    </div>
-  </div>
-</v-app>
+
+      <v-spacer></v-spacer>
+
+    <!--   <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn> -->
+    </v-app-bar>
+
+    <v-main>
+      <ListView/>
+    </v-main>
+  </v-app>
 </template>
+
 <script>
+import HelloWorld from './components/HelloWorld';
+import Course from './components/Course';
+import CoursesList from './components/CoursesList.vue';
+import ListView from './views/List-View.vue';
+import AddCourse from './components/AddCourse.vue';
+import AboutApp from './views/About-App.vue';
+
+
+
+
 export default {
-  name: "app"
+  name: 'App',
+
+  components: {
+    HelloWorld,
+    // AddCourse,
+    Course,
+    CoursesList,
+    ListView,
+    AddCourse,
+    AboutApp,
+},
+
+  data: () => ({
+    //
+  }),
+  methods: {
+    course(){
+      return{
+        component: "Course"
+      }
+    }
+  }
 };
 </script>
-
